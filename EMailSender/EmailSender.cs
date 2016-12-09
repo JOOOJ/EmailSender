226c732e794ff09ae37fe4ecba4eb6c701dc95ac
JOOOJ <jjjrachel@hotmail.com>
2016-12-09 17:05:59 +0800

    Init EmailSender project

---
+++ EMailSender/EmailSender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Mail;
+using System.IO;
+
+namespace EMailSender
+{
+    public class EmailSender
+    {
+       
+    }
+}
