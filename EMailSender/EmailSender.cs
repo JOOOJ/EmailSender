61131af922b8a721188da28072ebfc56a1e4ed7b
JOOOJ <jjjrachel@hotmail.com>
2016-12-09 17:10:01 +0800

    Add two APIs to send mail 1. One API: parameters with mail from,mail to,mail cc, mail subject and mail body 2. One API: paramters with mail BCC

--- EMailSender/EmailSender.cs
+++ EMailSender/EmailSender.cs
@@ -8,6 +8,77 @@ namespace EMailSender
 {
     public class EmailSender
     {
-       
+        private string m_SMTPServer;
+        public EmailSender(string SMTPServerIP)
+        {
+            m_SMTPServer = SMTPServerIP;
+        }
+
+        public void Send(string From,List<string> To, List<string> CC, string Subject,string Body)
+        {
+            MailMessage mail = SendPrepare(From, To, CC,Subject,Body);
+            StartSend(mail);
+        }
+
+        public void Send(string From, List<string> To, List<string> CC, string Subject, string Body,List<string> Bcc)
+        {
+            MailMessage mail = SendPrepare(From, To, CC, Subject, Body);
+            if(Bcc!=null)
+            {
+                foreach (string item in Bcc)
+                {
+                    mail.Bcc.Add(new MailAddress(item));
+                }
+            }
+            StartSend(mail);
+        }
+
+        private void StartSend(MailMessage mail)
+        {
+            using (SmtpClient client = new SmtpClient(m_SMTPServer))
+            {
+                try
+                {
+                    client.Send(mail);
+                }
+                catch (SmtpFailedRecipientsException ex)
+                {
+                    //foreach (var innerEx in ex.InnerExceptions)
+                    //{
+                    //    if (innerEx.StatusCode == SmtpStatusCode.MailboxBusy || innerEx.StatusCode == SmtpStatusCode.MailboxUnavailable)
+                    //    {
+                    //        client.Send(mail);
+                    //    }
+                    //}
+                    throw ex;
+                }
+            }         
+            
+        }
+
+        private static MailMessage SendPrepare(string From, List<string> To,List<string> CC, string Subject, string Body)
+        {
+            if (string.IsNullOrEmpty(From) || To == null || To.Count == 0 || string.IsNullOrEmpty(Subject))
+            {
+                throw new ArgumentException("Mail From or To is empty, please input them", "From,To,Subject", new Exception());
+            }
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress(From);
+            foreach (string item in To)
+            {
+                mail.To.Add(new MailAddress(item));
+            }
+            if (CC != null)
+            {
+                foreach (string item in CC)
+                {
+                    mail.CC.Add(new MailAddress(item));
+                }
+            }
+            mail.Subject = Subject;
+            mail.SubjectEncoding = Encoding.UTF8;
+            mail.Body = Body;
+            return mail;
+        }
     }
 }
